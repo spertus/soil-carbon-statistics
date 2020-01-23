7180749ce23451365fde6e10edbc7925dff84794
Jake Spertus <Jake@Jakes-MacBook-Pro.local>
2020-01-23 15:02:27 -0800

    added simulations for SOC concentrating along a ridge

--- sim_functions.R
+++ sim_functions.R
@@ -1,5 +1,54 @@
 library(tidyverse)
 
+################## function to simulate a % SOC surface #################
+#inputs:
+  #i: does nothing except allows for use with lapply() wrapper
+  #ridge: specify whether means should be a slope down the latitudes (no bias in transect sample), or a ridge along the center (bias in transect sample)
+#outputs:
+  #a 600 x 250 matrix of percent carbons along a surface, representing a 60m x 25m plot at a 10cm resolution
+simulate_truth <- function(i, ridge = FALSE){
+  #since silver lab plots are 60 x 25 meters, this gives a resolution of .1 meters
+  n_latitude <- 600
+  n_longitude <- 250
+  d <- n_latitude * n_longitude
+  
+  if(ridge == FALSE){
+    #first simulate a surface that is a "slope" with the mean decreasing linearly as we move down the surface
+    #mean varies across rows, it decreases linearly from .06 to .01
+    grid <- (1:n_latitude) / n_latitude
+    linear_mean <- .06 * (1-grid) + .01 * grid
+    mean_matrix <- matrix(data = rep(linear_mean, n_latitude), nrow = n_latitude, ncol = n_longitude)
+    #fix variance initially
+    var_matrix <- matrix(.0003, nrow = n_latitude, ncol = n_longitude)
+  }
+  if(ridge == TRUE){
+    grid <- (1:n_latitude) / n_latitude
+    #there is a ridge of high carbon along the diagonal, which drops off linearly on either side
+    #this is a very bad situation for a transect sample in terms of estimating absolute carbon 
+    #may not be as problematic for estimating a difference (treatment effect)
+    mean_matrix <-  matrix(data = 0, nrow = n_latitude, ncol = n_longitude)
+    for(i in 1:n_latitude){
+      for(j in 1:n_longitude){
+        mean_matrix[i,j] <- .1 * (abs(i - 2.4*j) / n_latitude) + .6 * (1 - abs(i - 2.4*j) / n_latitude)
+      }
+    }
+    var_matrix <- matrix(.0003, nrow = n_latitude, ncol = n_longitude)
+  }
+  
+  
+  #simulated carbon matrix
+  carbon_matrix <- matrix(0, nrow = n_latitude, ncol = n_longitude)
+  for(i in 1:n_latitude){
+    for(j in 1:n_longitude){
+      beta_params <- compute_beta_params(mu = mean_matrix[i,j], sigma_squared = var_matrix[i,j])
+      carbon_matrix[i,j] <- rbeta(n = 1, shape1 = beta_params[1], shape2 = beta_params[2])
+    }
+  }
+  carbon_matrix
+}
+
+
+
 ##### function to collect samples from a simulated surface ####
 #inputs:
 #surface: a simulated surface, i.e. a matrix or array with SOC concentrations at each point
