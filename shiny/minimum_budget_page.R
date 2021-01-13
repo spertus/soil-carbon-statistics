553da087060808e21b313e19582c20c101711397
Jake Spertus <jakespertus@gmail.com>
2021-01-13 08:51:46 -0800

    updated shiny app with bolder output text

--- shiny/minimum_budget_page.R
+++ shiny/minimum_budget_page.R
@@ -6,7 +6,7 @@ basicPage(
   h5("This app takes a standard error (estimation variance), costs, and parameters as inputs and returns the minimum budget that needs to estimate average SOC concentration. The number of cores (samples) and the assays to take is also returned.The currency used for costs and budget does not matter (results are scale invariant)."),
   sidebarPanel(
               numericInput("SE", "Input the desired estimation standard error (\\(SE\\))", value = .1),
-              numericInput("C_0_budget", "Input total fixed costs (\\(C_0\\))", value = 0),
+              numericInput("C_0_budget", "Input total fixed costs (\\(\\mbox{cost}_0\\))", value = 0),
               numericInput("cost_c_budget", "Input the cost of taking a single core (\\(\\mbox{cost}_c\\))", value = 30),
               numericInput("cost_P_budget", "Input the cost of preparing a single composite sample (\\(\\mbox{cost}_P\\))", value = 5),
               numericInput("cost_A_budget", "Input the cost of assaying a single composite sample (\\(\\mbox{cost}_A\\))", value = 5),
@@ -14,7 +14,10 @@ basicPage(
               numericInput("mu_budget", "Input the average plot concentration in %SOC (\\(\\mu\\))", value = 2.5),
               numericInput("sigma_delta_budget", "Input the assay uncertainty (\\(\\sigma_\\delta\\))", value = 0.25)
   ),
-  textOutput("budget"),
-  textOutput("n"),
-  textOutput("k")
+  mainPanel(
+    h3("Results"),
+    span(textOutput("budget"), style="color:blue; font-size:20px"),
+    span(textOutput("n"), style="color:blue; font-size:20px"),
+    span(textOutput("k"), style="color:blue; font-size:20px")
+  )
 )
