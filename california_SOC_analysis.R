c8331a39b3298c4f1eaf12c24b325a9d8eef2bb1
Jake Spertus <jakespertus@gmail.com>
2021-02-04 12:06:02 -0800

    added script to analyze data from Paicines ranch and a variety of California croplands

---
+++ california_SOC_analysis.R
@@ -0,0 +1,200 @@
+library(tidyverse)
+library(readxl)
+library(permuter)
+#permuter is used for permutation tests. It can be installed from Github: https://github.com/statlab/permuter
+
+########### read in data from excel spreadsheet(s) ##############
+#data from Paicines Ranch
+#from the original data sent to me by Paige Stanley, I deleted a few free-floating cells on the Rangeland_BD sheet. I also changed 'dulk_density_g/cm3' to 'bulk_density_g/cm3' on Rangeland_BD and Cropland_BD.
+paicines_master <- read_excel("../Data/Heterogeneity_Master_PS_02032021.xlsx", sheet = "Rangeland_All_soliTOC") %>%
+  rename(SOC = TOC) %>%
+  mutate(SOC = ifelse(SOC == "NA", NA, SOC)) %>%
+  mutate(TIC = ifelse(TIC == "NA", NA, TIC)) %>%
+  mutate(TC = ifelse(TC == "NA", NA, TC)) %>%
+  mutate(SOC = as.numeric(SOC), TIC = as.numeric(TIC), TC = as.numeric(TC), sample_number = as.numeric(sample_number))
+
+paicines_solitoc_reps <- read_excel("../Data/Heterogeneity_Master_PS_02032021.xlsx", sheet = "Rangeland_Reps_soliTOC") %>%
+  rename(SOC = TOC) %>%
+  mutate(SOC = ifelse(SOC == "NA", NA, SOC)) %>%
+  mutate(SOC = as.numeric(SOC), sample_number = as.numeric(sample_number)) %>%
+  select(-TOC_replicate_avg, -TOC_replicate_stdev) %>%
+  mutate(machine = "solitoc")
+
+paicines_costech_reps <- read_excel("../Data/Heterogeneity_Master_PS_02032021.xlsx", sheet = "Rangeland_Reps_Costech") %>%
+  rename(SOC = TOC) %>%
+  mutate(SOC = ifelse(SOC == "NA", NA, SOC)) %>%
+  mutate(SOC = as.numeric(SOC), sample_number = as.numeric(sample_number)) %>%
+  select(-replicate_avg, -replicate_stdev) %>%
+  mutate(machine = "costech")
+
+paicines_BD <- read_excel("../Data/Heterogeneity_Master_PS_02032021.xlsx", sheet = "Rangeland_BD") %>%
+  rename('bd' = 'bulk_density_g/cm3') %>%
+  mutate(bd = as.numeric(bd))
+
+#data from various croplands around California
+cropland_master <- read_excel("../Data/Heterogeneity_Master_PS_02032021.xlsx", sheet = "Cropland_All_Costech") %>%
+  rename(SOC = TOC) %>%
+  mutate(SOC = ifelse(SOC == "NA", NA, SOC)) %>%
+  mutate(SOC = as.numeric(SOC), sample_number = as.numeric(sample_number))
+
+cropland_solitoc_reps <- read_excel("../Data/Heterogeneity_Master_PS_02032021.xlsx", sheet = "Cropland_Reps_soliTOC") %>%
+  rename(SOC = TOC) %>%
+  mutate(SOC = ifelse(SOC == "NA", NA, SOC)) %>%
+  mutate(SOC = as.numeric(SOC), sample_number = as.numeric(sample_number)) %>%
+  select(-TOC_replicate_avg, -TOC_replicate_stdev) %>% 
+  mutate(machine = "solitoc")
+
+cropland_costech_reps <- read_excel("../Data/Heterogeneity_Master_PS_02032021.xlsx", sheet = "Cropland_Reps_Costech") %>%
+  rename(SOC = TOC) %>%
+  mutate(SOC = ifelse(SOC == "NA", NA, SOC)) %>%
+  mutate(SOC = as.numeric(SOC), sample_number = as.numeric(sample_number)) %>%
+  select(-replicate_avg, -replicate_stdev) %>% 
+  mutate(machine = "costech")
+
+cropland_BD <- read_excel("../Data/Heterogeneity_Master_PS_02032021.xlsx", sheet = "Cropland_BD")  %>%
+  rename('bd' = 'bulk_density_g/cm3') %>%
+  mutate(bd = as.numeric(bd))
+
+
+
+###################### SOC concentration and BD in space ######################
+
+#stacked histograms for Paicines data
+ggplot(paicines_master, aes(SOC)) +
+  geom_histogram() +
+  facet_grid(depth ~ transect)
+
+
+#stacked histograms for cropland data
+ggplot(cropland_master, aes(SOC)) +
+  geom_histogram() +
+  facet_grid(depth ~ site)
+
+
+#summary tables
+paicines_summary <- paicines_master %>% 
+  group_by(depth, transect) %>%
+  summarize(mean_SOC = mean(SOC, na.rm = T), sd_SOC = sd(SOC, na.rm = T))
+
+cropland_summary <- cropland_master %>% 
+  group_by(depth, site) %>%
+  summarize(mean_SOC = mean(SOC, na.rm = T), sd_SOC = sd(SOC, na.rm = T))
+
+
+#linear models
+paicines_model <- lm(SOC ~ transect*depth, data = paicines_master)
+cropland_model <- lm(SOC ~ site*depth, data = cropland_master)
+
+anova(paicines_model)
+anova(cropland_model)
+
+
+#bulk densities
+#NOTE: histograms are not disaggregated by location, which drives variation
+ggplot(paicines_BD, aes(bd)) +
+  geom_histogram() +
+  facet_grid(depth ~ .)
+
+ggplot(cropland_BD, aes(bd)) +
+  geom_histogram() +
+  facet_grid(depth ~ .)
+
+paicines_summary_bd <- paicines_BD %>%
+  group_by(depth, transect) %>%
+  summarize(mean_bd = mean(bd, na.rm = TRUE), sd_bd = sd(bd, na.rm = TRUE))
+
+cropland_summary_bd <- cropland_BD %>%
+  group_by(depth, site) %>%
+  summarize(mean_bd = mean(bd, na.rm = TRUE), sd_bd = sd(bd, na.rm = TRUE))
+
+############## replicates and assay error ##############
+assay_error_solitoc <- paicines_solitoc_reps %>% 
+  bind_rows(cropland_solitoc_reps) %>%
+  group_by(site, sample_number) %>%
+  summarize(
+    machine = first(machine),
+    depth = first(depth), 
+    sigma_delta = sqrt(var(SOC) / (mean(SOC)^2 - var(SOC)/n())),
+    SOC = mean(SOC)
+  ) %>%
+  ungroup() %>%
+  na.omit()
+
+assay_error_costech <- paicines_costech_reps %>% 
+  bind_rows(cropland_costech_reps) %>%
+  group_by(site, sample_number) %>%
+  summarize(
+    machine = first(machine),
+    depth = first(depth), 
+    sigma_delta = sqrt(var(SOC) / (mean(SOC)^2 - var(SOC)/n())),
+    SOC = mean(SOC)
+  ) %>%
+  na.omit()
+
+assay_error_long <- bind_rows(assay_error_solitoc, assay_error_costech)
+
+#compare average errors on paicines samples
+ggplot(assay_error_long, aes(sigma_delta*100, fill = machine)) +
+  geom_density(alpha = .5) +
+  xlim(0,10) +
+  xlab("Percent Error")
+
+median(assay_error_solitoc$sigma_delta)
+median(assay_error_costech$sigma_delta)
+
+#compare measurements
+assay_error_wide <- assay_error_long %>%
+  pivot_wider(names_from = "machine", values_from = c("sigma_delta", "SOC"))
+
+ggplot(assay_error_wide, aes(x = SOC_solitoc, y = SOC_costech, color = site)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0) +
+  xlab("%SOC SoliTOC") +
+  ylab("%SOC Costech")
+
+median(assay_error_solitoc$SOC)
+median(assay_error_costech$SOC)
+
+
+#non-parametric permutation test for differences in measurement
+B <- 10000
+
+#nonparametric analysis of no difference in labs/machines:
+reps_long <- bind_rows(paicines_solitoc_reps, cropland_solitoc_reps) %>%
+  select(-TIC, -TC) %>%
+  bind_rows(paicines_costech_reps, cropland_costech_reps) %>%
+  na.omit() %>%
+  mutate(sample_ID = paste(site, sample_number, sep = "_"))
+
+
+strata <- as_factor(reps_long$sample_ID)
+num_strata <- length(unique(strata))
+#test statistic is difference in means between labs
+diff_means <- rep(0, num_strata)
+null_distributions <- matrix(0, ncol = num_strata, nrow = B)
+p_values <- rep(0, num_strata)
+#iterate across strata (samples)
+for(k in 1:num_strata){
+  solitoc_soc <- reps_long$SOC[reps_long$machine == "solitoc" & strata == levels(strata)[k]]
+  costech_soc <- reps_long$SOC[reps_long$machine == "costech" & strata == levels(strata)[k]]
+  diff_means[k] <- mean(solitoc_soc) - mean(costech_soc)
+  null_distributions[,k] <- two_sample(x = solitoc_soc, y = costech_soc, reps = B)
+  p_values[k] <- t2p(tst = diff_means[k], distr = null_distributions[,k], alternative = "two-sided")
+}
+combined_p_value <- npc(diff_means, distr = null_distributions, combine=  "fisher", alternatives = "two-sided")
+
+
+#average inorganic carbon is interesting as a predictor of difference between costech and solitoc
+avg_TIC <- bind_rows(paicines_solitoc_reps, cropland_solitoc_reps) %>%
+  mutate(sample_ID = paste(site, sample_number, sep = "_")) %>%
+  group_by(sample_ID) %>%
+  summarize(avg_TIC = mean(TIC))
+
+p_value_frame <- data.frame(sample_ID = levels(strata), diff_mean = diff_means, p_value = p_values) %>%
+  left_join(avg_TIC, by = "sample_ID")
+
+ggplot(p_value_frame, aes(x = log10(avg_TIC), y = diff_mean, color = p_value < .05)) +
+  geom_point()
+
+############### costs and optimal compositing #############
+
