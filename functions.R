f0ce5273919d9ad9e655477c3bf8dfb8ee15431e
Jake Spertus <jakespertus@gmail.com>
2021-10-21 16:49:39 -0700

    added higher sample sizes to simulations

--- functions.R
+++ functions.R
@@ -1191,17 +1191,16 @@ two_sample_anderson_test <- function(n, pop_1, pop_2, resample){
   #B: a positive integer, the number of Monte Carlo iterations to be used in running the test
   #method: a string, the method of combining the two one-sample tests into a two-sample test of equality
     #Sidak: compute a 1-sqrt(1-alpha) upper and lower bound (respectively) and see if they overlap
-    #Fisher: compute a combined p-value of whether both means are equal to a particular mu_0, then maximize this over mu_0 in [a,b]
-      #if [a,b] is [0,1] this test is always valid, but may be faster if the mean is known to lie in a more narrow interval 
-  #mu_0: interval of null means to search over for Fisher's test
-  #pval: a boolean, if TRUE return a pvalue (only works for Fisher), if FALSE return a boolean indicating rejection at level alpha 
-two_sample_LMT_test <- function(sample_1, sample_2, alpha, B = 1000, method = "Sidak", pval = FALSE){
-  if(method == "Sidak"){
+    #Fisher: compute a combined p-value using Fisher's combining function of whether both means are equal to a particular mu_0, then maximize this over possible values of mu_0
+    #Liptak: compute a combined p-value using Liptak's combining function
+  #pval: a boolean, if TRUE return a pvalue (only works for Fisher or Liptak), if FALSE return a boolean indicating rejection at level alpha 
+two_sample_LMT_test <- function(sample_1, sample_2, alpha, B = 1000, method = "sidak", pval = FALSE){
+  if(method == "sidak"){
     upper_1 <- LMT_CI(x = sample_1, alpha = 1-sqrt(1-alpha), B = B, side = "upper")
     lower_2 <- LMT_CI(x = sample_2, alpha = 1-sqrt(1-alpha), B = B, side = "lower")
     reject <- ifelse(upper_1 < lower_2, TRUE, FALSE)
     reject
-  } else if(method == "Fisher"){
+  } else if(method %in% c("fisher","liptak","tippett")){
     n_1 <- length(sample_1)
     n_2 <- length(sample_2)
     x_1 <- sample_1
@@ -1209,24 +1208,39 @@ two_sample_LMT_test <- function(sample_1, sample_2, alpha, B = 1000, method = "S
     ms_1 <- rep(NA, B)
     ms_2 <- rep(NA, B)
     for(b in 1:B){
-      Z <- rexp(n_1 + 1)
-      D <- Z / sum(Z)
-      ms_1[b] <- sum(D * c(x_1, 1))
-      ms_2[b] <- sum(D * c(x_2, 0))
+      Z_1 <- rexp(n_1 + 1)
+      D_1 <- Z_1 / sum(Z_1)
+      Z_2 <- rexp(n_2 + 1)
+      D_2 <- Z_2 / sum(Z_2)
+      ms_1[b] <- sum(D_1 * c(x_1, 1))
+      ms_2[b] <- sum(D_2 * c(x_2, 0))
     }
-    #it might be possible to find the maximizing mu_0 deterministically based on the draws...
-    #sorted_ms_1 <- sort(ms_1, decreasing = T)
-    #sorted_ms_2 <- sort(ms_2, decreasing = F)
-    #closest_point <- which.min(abs(sorted_ms_1 - sorted_ms_2))
-    #opt_mu_0 <- (sorted_ms_1[closest_point] + sorted_ms_2[closest_point])/2
+    if(method == "fisher"){
+      combined_p <- function(mu_0){
+        p_1 <- mean(c(ms_1 >= mu_0, TRUE))
+        p_2 <- mean(c(ms_2 <= mu_0, TRUE))
+        combined_test_stat <- -2 * (log(p_1) + log(p_2))
+        p_val <- pchisq(q = combined_test_stat, df = 4, lower.tail = FALSE)
+        p_val
+      }
+    } else if(method == "liptak"){
+      combined_p <- function(mu_0){
+        p_1 <- mean(c(ms_1 >= mu_0, TRUE))
+        p_2 <- mean(c(ms_2 <= mu_0, TRUE))
+        combined_test_stat <- qnorm(1 - p_1) + qnorm(1 - p_2)
+        p_val <- pnorm(q = combined_test_stat, sd = 2, lower.tail = FALSE)
+        p_val
+      }
+    } else{
+      combined_p <- function(mu_0){
+        p_1 <- mean(c(ms_1 >= mu_0, TRUE))
+        p_2 <- mean(c(ms_2 <= mu_0, TRUE))
+        combined_test_stat <- min(p_1, p_2)
+        p_val <- pbeta(q = combined_test_stat, shape1 = 1, shape2 = 2, lower.tail = TRUE)
+        p_val
+      }
+    }
     
-    combined_p <- function(mu_0){
-      p_1 <- mean(c(ms_1 >= mu_0, TRUE))
-      p_2 <- mean(c(ms_2 <= mu_0, TRUE))
-      combined_test_stat <- -2 * (log(p_1) + log(p_2))
-      p_val <- pchisq(q = combined_test_stat, df = 4, lower.tail = FALSE)
-      p_val
-    }
     max_p_val <- optimize(combined_p, lower = min(c(ms_1, ms_2)), upper = max(c(ms_1, ms_2)), maximum = TRUE)$objective
     if(pval){
       max_p_val
@@ -1234,7 +1248,7 @@ two_sample_LMT_test <- function(sample_1, sample_2, alpha, B = 1000, method = "S
       ifelse(max_p_val < alpha, TRUE, FALSE)
     }
   } else{
-    stop("Input a valid method: Sidak or Fisher")
+    stop("Input a valid method: sidak, fisher, liptak, or tippett")
   }
 }
 
