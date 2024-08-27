9163b37fca41095dbd7e227f1f312c1c6cdb5ab0
spertus <jakespertus@gmail.com>
2024-08-27 11:55:41 -0700

    minor updates to shiny app

--- shiny/compute_power_page.R
+++ shiny/compute_power_page.R
@@ -1,21 +1,22 @@
 
 basicPage(
   titlePanel("Compute power for fixed sample sizes"),
-  h5("Given means, spatial heterogeneities, and sample sizes for two plots (or sampling times), this app computes the power of a two-sample t-test where the null is no difference between average plot concentrations. Assumes independent simple random sampling from each plot. The validity of the test relies on SOC being approximately normally distributed in each plot."),
+  h5("Given means, spatial heterogeneities, and sample sizes for two plots (or sampling times), this app computes the power of a two-sample t-test where the null is no difference between average plot concentrations. Option to include assay error and adjust the number of assays (which may be less than the sample size when compositing is used). If the assay variability is 0, the number of assays is irrelevant. Assumes independent simple random sampling from each plot. The validity of the test relies on SOC being approximately normally distributed in each plot."),
   sidebarPanel(
              numericInput("alpha_power", "Input the significance level of the test (\\(\\alpha\\))", value = 0.05),
              numericInput("mu_1_power", "Input true average %SOC in plot 1 (\\(\\mu_1\\))", value = 1),
              numericInput("mu_2_power", "Input true average %SOC in plot 2 (\\(\\mu_2\\))", value = 2),
              numericInput("n_1_power", "Input the number of cores from plot 1 (\\(n_1\\))", value = 30),
-             numericInput("k_1_power",  "Input the number of assays of cores from plot 1 (\\(k_1\\))", value = 30),
              numericInput("n_2_power", "Input the number of cores from plot 2 (\\(n_2\\))", value = 30),
-             numericInput("k_2_power",  "Input the number of assays of cores from plot 2 (\\(k_2\\))", value = 30),
-             numericInput("sigma_p_1_power", "Input heterogeneity of %SOC in plot 1 (\\(\\sigma_{p1}\\))", value = 1.00),
-             numericInput("sigma_p_2_power", "Input heterogeneity of %SOC in plot 2 (\\(\\sigma_{p2}\\))", value = 1.00),
-             numericInput("sigma_delta_power", "Input assay uncertainty (\\(\\sigma_\\delta\\))", value = 0)
+             numericInput("sigma_p_1_power", "Input spatial heterogeneity of %SOC in plot 1 (\\(\\sigma_{p1}\\))", value = 1.00),
+             numericInput("sigma_p_2_power", "Input spatial heterogeneity of %SOC in plot 2 (\\(\\sigma_{p2}\\))", value = 1.00),
+             numericInput("sigma_delta_power", "Input assay variability (\\(\\sigma_\\delta\\))", value = 0),
+             numericInput("k_1_power",  "Input the number of assays of cores from plot 1 (\\(k_1\\))", value = 30),
+             numericInput("k_2_power",  "Input the number of assays of cores from plot 2 (\\(k_2\\))", value = 30)
   ),
   mainPanel(
     h3("Results"),
+    span(textOutput("power_effectsize"), style="color:blue; font-size:20px"),
     span(textOutput("power"), style="color:blue; font-size:20px")
   )
 )
--- shiny/explanation_page.R
+++ shiny/explanation_page.R
@@ -3,14 +3,15 @@ library(shiny)
 
 basicPage(
   h3("Explanation"),
-  h6("This collection of shiny apps is designed to assist researchers with soil sampling design.The apps can compute power for SOC change detection, recommend sample sizes, and maximize precision when compositing is used to reduce the number of assays. "),
+  h6("This collection of shiny apps is designed to assist researchers with soil sampling design. The apps can compute power for SOC change detection, recommend sample sizes, and maximize precision when compositing is used to reduce the number of assays. "),
+  h6("Select an app from the drop down menu above. More information about each app can be found on the corresponding page."),
   
   h3("Notation"),
   h6("The apps refer to the following notation for plot parameters:"),
   h6("\\(\\cdot~~ \\mu \\): The plot average is the average percent soil organic carbon (%SOC) over the plot."),
   h6("\\(\\cdot~~ \\sigma_{p}\\): The spatial heterogeneity is the standard deviation of %SOC over the plot."),
   h6("\\(\\cdot~~ \\sigma_{\\delta}\\): The assay error is the average deviation of the assay from the true SOC concentration. It is closely related to the average percent error in assay. For example, if \\(\\sigma_{\\delta} = 0.1\\) we would expect most assays of a single sample to be within about 10% of the true SOC concentration of that sample. If \\(\\sigma_{\\delta} = 0\\), there is no assay error."),
-  h6("For more details refer to Spertus (2021)\\, available at:"), 
+  h6("For more details refer to Spertus (2021) \"Optimal Sampling and Assay for Estimating Soil Organic Carbon\", available at:"), 
      tagList("https://www.scirp.org/journal/paperinformation?paperid=107467")
 )
 
